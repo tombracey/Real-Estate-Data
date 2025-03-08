cfb614f8698ce92085988ced2a5dd3fc4e0b4885
Tom Bracey <tombracey22@gmail.com>
2025-03-08 21:11:06 +0000

    changed plan on map visual

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 Datasets:
 1. Transaction information from the Land Registry
 2. Energy performance and characteristics data
-3. 50,000 rows of addresses
+3. 50,000 rows of Exeter addresses
 
 Ideas:
 1. Price model for property, based on size, bedrooms, area etc.
-2. Energy certification correlation with property age
-3. Commercial properties in the area??
+2. Map visual of property upgrade potential
+
+
+
+To run the bash script with SQL queries:
+`echo "export PGPASSWORD='<your_password_here>'" > .env`
--- join_csvs.sh
+++ join_csvs.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 DB_NAME="exeter_epc_data"
-CSV_PATH="./data/addressbase_sample.csv"
+AB_CSV_PATH="./data/addressbase_sample.csv"
+EPC_CSV_PATH="./data/epc_data.csv"
 PG_PASSWORD="1234"
 
 export PGPASSWORD=$PG_PASSWORD
+
+# Import address data
 
 psql -U postgres -d "$DB_NAME" -c "
 DROP TABLE IF EXISTS temp_addressbase;
@@ -16,8 +19,27 @@ CREATE TABLE temp_addressbase (
 "
 
 echo "Importing CSV..."
-psql -U postgres -d "$DB_NAME" -c "\COPY temp_addressbase (UKPRN, latitude, longitude) FROM '$CSV_PATH' DELIMITER ',' CSV HEADER;"
+psql -U postgres -d "$DB_NAME" -c "\COPY temp_addressbase (UKPRN, latitude, longitude) FROM '$AB_CSV_PATH' DELIMITER ',' CSV HEADER;"
 
 psql -U postgres -d "$DB_NAME" -c "SELECT * FROM temp_addressbase LIMIT 5;"
+
+# Import EPC data
+
+psql -U postgres -d "$DB_NAME" -c "
+DROP TABLE IF EXISTS temp_epc_data;
+CREATE TABLE temp_epc_data (
+    UPRN TEXT PRIMARY KEY,
+    LIGHTING_COST_CURRENT NUMERIC,
+    LIGHTING_COST_POTENTIAL NUMERIC,
+    HEATING_COST_CURRENT NUMERIC,
+    HEATING_COST_POTENTIAL NUMERIC,
+    HOT_WATER_COST_CURRENT NUMERIC,
+    HOT_WATER_COST_POTENTIAL NUMERIC,
+    ADDRESS TEXT
+);
+"
+
+echo "Importing epc_data.csv..."
+psql -U postgres -d "$DB_NAME" -c "\COPY temp_epc_data (UPRN, LIGHTING_COST_CURRENT, LIGHTING_COST_POTENTIAL, HEATING_COST_CURRENT, HEATING_COST_POTENTIAL, HOT_WATER_COST_CURRENT, HOT_WATER_COST_POTENTIAL, ADDRESS) FROM '$EPC_CSV_PATH' DELIMITER ',' CSV HEADER;"
 
 unset PGPASSWORD
