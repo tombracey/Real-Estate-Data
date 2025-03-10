6b9822aa88311fcccdfdc232b6829f4d0cc29caa
Tom Bracey <tombracey22@gmail.com>
2025-03-10 21:01:11 +0000

    comment for explanation

--- download_land_registry.py
+++ download_land_registry.py
@@ -1,3 +1,5 @@
+# Once the `requests` module is installed, running this file will download the last month of Land Registry data
+
 import requests
 
 url = "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-monthly-update-new-version.csv"
